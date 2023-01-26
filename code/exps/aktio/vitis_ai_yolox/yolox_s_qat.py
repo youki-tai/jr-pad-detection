0cb1a71100fbf419182e6d8307b6b433161e8fee
shimoshida <moshimoshimoshida@gmail.com>
2023-01-26 15:11:02 +0900

    add official model

--- code/exps/aktio/vitis_ai_yolox/yolox_s_qat.py
+++ code/exps/aktio/vitis_ai_yolox/yolox_s_qat.py
@@ -1,5 +1,4 @@
 import os
-
 from yolox.exp import ExpDeployQat as MyExp
 
 
@@ -30,40 +29,3 @@ class Exp(MyExp):
         self.eval_interval = 1
         self.calib_dir = 'quantized'
         
-
-    # def get_model(self):
-    #     from yolox.models import YOLOX, YOLOXHead
-    #     # 1st conv: in-channel=12
-    #     from yolox.models.yolo_pafpn import YOLOPAFPN
-    #     # 1st conv: in-channel=3
-    #     # from yolox.models.yolo_pafpn_deploy import YOLOPAFPN
-
-    #     def init_yolo(M):
-    #         for m in M.modules():
-    #             import torch.nn as nn
-    #             if isinstance(m, nn.BatchNorm2d):
-    #                 m.eps = 1e-3
-    #                 m.momentum = 0.03
-
-    #     if getattr(self, "model", None) is None:
-    #         in_channels = [256, 512, 1024]
-    #         backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, act=self.act)
-    #         head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, act=self.act)
-    #         self.model = YOLOX(backbone, head)
-
-    #     self.model.apply(init_yolo)
-    #     self.model.head.initialize_biases(1e-2)
-
-    #     if self.is_qat:
-    #         import torch
-    #         from pytorch_nndct import QatProcessor
-    #         if self.float_ckpt:
-    #             ckpt = torch.load(self.float_ckpt)
-    #             print('Loading float model weight for QAT from {}'.format(self.float_ckpt))
-    #             self.model.load_state_dict(ckpt["model"])
-    #         dummy_input = torch.randn([1, 3, 640, 640], dtype=torch.float32).cuda()
-    #         self.qat_processor = QatProcessor(self.model, dummy_input, bitwidth=8, mix_bit=False)
-    #         self.model = self.qat_processor.trainable_model(calib_dir=self.calib_dir)
-
-    #     return self.model
-
