3a192a11c33bba8de8c609fcccbd062be2e900c3
Youki Sada <sada.yuuki@tokyo-ai.co.jp>
2023-12-04 23:13:36 +0900

    file added

---
+++ kv260-deploy/cpp/vitis_ai_yolovx_wr.cpp
@@ -0,0 +1,130 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <Eigen/Core>
+#include <opencv2/core/eigen.hpp>
+#include <vitis/ai/yolovx.hpp>
+
+
+
+#include <vitis/ai/nnpp/yolovx.hpp>
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+#include <pybind11/stl.h>
+#include <pybind11/complex.h>
+#include <pybind11/functional.h>
+
+#define VERSION "20221224"
+
+using Image8bpp =
+  Eigen::Matrix<uint8_t,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor>;
+
+namespace py = pybind11;
+
+class yolovx_wr{
+  std::unique_ptr<vitis::ai::YOLOvX>  yolovx_obj;
+  vitis::ai::YOLOvXResult  result;
+  std::vector< vitis::ai::YOLOvXResult>   results;  
+public:
+  yolovx_wr(const std::string &name){
+    yolovx_obj = vitis::ai::YOLOvX::create(name);
+  }
+  auto version(){
+    return VERSION;
+  }
+
+  auto detection(const Image8bpp& x){
+    cv::Mat frame;
+    cv::eigen2cv(x,frame);
+#ifdef __DEBUG__
+    printf("b %d %d %d\n",frame.cols,frame.rows,frame.channels());
+#endif
+    frame = frame.reshape(3, frame.rows);
+#ifdef __DEBUG__
+    printf("a %d %d %d\n",frame.cols,frame.rows,frame.channels());
+#endif
+    result = yolovx_obj->run(frame);
+  }
+  
+  auto detections(const std::vector< Image8bpp> & x){
+    std::vector< cv::Mat>  frames(x.size());
+    for (unsigned int i=0;i<x.size() ;i++){
+      cv::eigen2cv(x[i],frames[i]);
+#ifdef __DEBUG__
+      printf("b %d %d %d\n",frames[i].cols,frames[i].rows,frames[i].channels());
+#endif
+      
+      frames[i] = frames[i].reshape(3,frames[i].rows);
+#ifdef __DEBUG__
+    	printf("a %d %d %d\n",frames[i].cols,frames[i].rows,frames[i].channels());
+#endif
+    }
+
+    results = yolovx_obj->run(frames);
+  }
+  auto num_results(){
+    return results.size();
+  }
+  
+  auto num(){
+    return result.bboxes.size();
+  }
+  auto bbox(int i){
+    std::vector<float> bbox_pos(10);
+    bbox_pos[0] = result.bboxes[i].box[0]; // x0
+    bbox_pos[1] = result.bboxes[i].box[1]; // y0 
+    bbox_pos[2] = result.bboxes[i].box[2]; // x1
+    bbox_pos[3] = result.bboxes[i].box[3]; // y1
+    bbox_pos[4] = result.bboxes[i].label;  // label
+    bbox_pos[5] = result.bboxes[i].score;  // score
+    return bbox_pos;
+  }
+  
+  auto bbox_list(){
+    std::vector< std::vector<float> > bbox_lst(num());
+    for (unsigned int i=0;i<num();i++){
+      bbox_lst[i] = bbox(i);
+    }
+    return bbox_lst;
+  }
+
+  
+  auto num_bboxes(int i){
+    return results[i].bboxes.size();
+  }
+  
+  auto bboxes(int res,int i){
+    std::vector<float> bbox_pos(10);
+    bbox_pos[0] = results[res].bboxes[i].box[0]; // x0
+    bbox_pos[1] = results[res].bboxes[i].box[1]; // y0 
+    bbox_pos[2] = results[res].bboxes[i].box[2]; // x1
+    bbox_pos[3] = results[res].bboxes[i].box[3]; // y1
+    bbox_pos[4] = results[res].bboxes[i].label;  // label
+    bbox_pos[5] = results[res].bboxes[i].score;  // score
+    return bbox_pos;
+  }
+  
+
+
+  
+};
+
+PYBIND11_MODULE(vitis_ai_yolovx_wr,m) {
+  py::class_<yolovx_wr>(m,"yolovx_wr")
+    .def(py::init<const std::string &>())
+    .def("version",&yolovx_wr::version)
+    .def("detection",&yolovx_wr::detection)
+    .def("detections",&yolovx_wr::detections)
+    .def("num_results",&yolovx_wr::num_results)
+    .def("num",&yolovx_wr::num)
+    .def("bbox",&yolovx_wr::bbox)
+    .def("bbox_list",&yolovx_wr::bbox_list)
+    .def("num_bboxes",&yolovx_wr::num_bboxes)
+    .def("bboxes",&yolovx_wr::bboxes)
+    ;
+}
+    
+  
--- kv260-deploy/main.py
+++ kv260-deploy/main.py
@@ -1,44 +1,55 @@
-
-
 import cv2
 import vitis_ai_yolovx_wr
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture("xmodel_bolt/bolt_sample_416.avi")
-yolovx_obj = vitis_ai_yolovx_wr.yolovx_wr("models/official-yolox-m/tsd_yolox_pt.xmodel")
+cap = cv2.VideoCapture("test.webm")
+#frame = cv2.imread('val2017/00198.png')
+yolovx_obj = vitis_ai_yolovx_wr.yolovx_wr("compiled-yolox-m/yolox-m.xmodel")
+#yolovx_obj = vitis_ai_yolovx_wr.yolovx_wr("models/official-yolox-m/tsd_yolox_pt.xmodel")
 
+#import time
+colors = [(255,0,0),(0,255,0),(0,0,255)]
+names = ["fixed_parts", "pad_b", "pad_a"]
+mm_per_pix = 373./1920
+off = 20
 while True:
     retval,frame = cap.read()
     if not retval:
         cv2.waitKey(10)
         continue
-    
-    ## 画サイズ取得
-    height,width = frame.shape[0],frame.shape[1]
+    #height,width = frame.shape[0],frame.shape[1]
 
-    ## 2次元に変換してC++にわたす
+    ## prepare input mat for DPU
     frame_2d = frame.reshape(frame.shape[0],frame.shape[1]*frame.shape[2])
-    yolovx_obj.detections(frame_2d) ## 2次元
+    #t0 = time.time()
+    yolovx_obj.detection(frame_2d)
+    #t = time.time() - t0
+    #print(t/100*1000, 'ms')
+    #print(100/t, 'fps')
 
-    ## BBOX描画
+    ## draw BBOX
     for roi in range(yolovx_obj.num()):
         bbox = yolovx_obj.bbox(roi)
-        xmin = int(bbox[0])
-        ymin = int(bbox[1])
-        xmax = int(bbox[2])
-        ymax = int(bbox[3])
-        cv2.rectangle(frame,
-                      (xmin,ymin),
-                      (xmax,ymax),
-                      (0,255,0),
-                      3,
-                      3,
-                      0)
+        xmin, ymin = int(bbox[0]), int(bbox[1])
+        xmax, ymax = int(bbox[2]), int(bbox[3])
+        cls_num = int(bbox[4])
+        conf = bbox[5]
+        print(cls_num)
+        if cls_num==0: offy = 20
+        else:          offy = -45
+        cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), colors[cls_num],
+                thickness=2, lineType=cv2.LINE_4, shift=0)
+        cv2.putText(frame, f'cls: {names[cls_num]}', (xmin, ymin+offy), cv2.FONT_HERSHEY_PLAIN,
+                fontScale=1.5, color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
+        cv2.putText(frame, f'conf: {conf*100:.1f}%', (xmin, ymin+offy+off*1), cv2.FONT_HERSHEY_PLAIN,
+                fontScale=1.5, color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
+        if cls_num!=0:
+            length = mm_per_pix * (bbox[2]-bbox[0])
+            cv2.putText(frame, f'length: {length:.1f}mm', (xmin, ymin+offy+off*2), cv2.FONT_HERSHEY_PLAIN,
+                    fontScale=1.5, color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
         
     cv2.imshow("yolovx",frame)
     if cv2.waitKey(1)=='q':
         break
-    
-
-    
 
